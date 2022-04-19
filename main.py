11a010527310eb886c22c959b2c34f958a203181
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-04-19 03:11:48 +0100

    Hello SideHustle.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# Different methods of writing strings
+print('Hello SideHustle')
+print("Hello SideHustle")
+print('''Hello SideHustle''')
+print('My name is '
+      'Oliveth Ndubuka')
+print('This is my first attempt at python')
+# A triple quotation mark is otherwise known as DocString
+
+print('''These are my best frameworks:
+            1. Django
+            2. RestFul APIs''')
+
+# \n is called New line
+# \t is called a new Tab, it could be '\t', '\t\t'
+
+print('These are my best frameworks: \n \t\t\t'
+      '1. Mern')
+
+print('\n This is my Father\'s car')
+print('''These are my hobbies: \n\t\t\t'''
+      '1. Singing \n\t\t\t'
+      '2. Writing clean codes \n\t\t\t'
+      '3. Research work \n\t\t\t')
